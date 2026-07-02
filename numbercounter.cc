7cbc750e47c6ecadbc2663568011dbd64dd4b2f8
gnix22 <gjnix22@gmail.com>
2026-07-02 00:05:06 -0400

    got a numbercounter for fun, try to figure out logic for 100.

---
+++ numbercounter.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+int main() {
+std::vector<unsigned char> tape(30000, 0);
+auto ptr = tape.begin();
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+while (*ptr) {
+++ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+--ptr;
+--*ptr;
+}
+while (*ptr) {
+--*ptr;
+}
+++ptr;
+++ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+++*ptr;
+while (*ptr) {
+--ptr;
+std::cout << *ptr;
+++*ptr;
+++ptr;
+--*ptr;
+}
+return 0;
+}
